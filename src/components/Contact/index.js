02c49172a3a55a6d0ffe77b08d84dbf073902955
boonprakit <boonprakit@proteus-tech.com>
2018-05-04 02:51:20 +0700

    show categories show contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -44,6 +44,7 @@ class Contact extends React.Component {
                 </div>
 
                 <div className="row sideBar">
+                    <a className="heading-name-meta" style={{ padding: '10px', borderBottom: '0.5px solid #ccc', backgroundColor: '#fbfbfb' }}>Favorites</a>
                     <div className="row sideBar-body">
                         <div className="col-sm-3 col-xs-3 sideBar-avatar">
                             <div className="avatar-icon">
@@ -63,6 +64,88 @@ class Contact extends React.Component {
                             </div>
                         </div>
                     </div>
+                    <div className="row sideBar-body">
+                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
+                            <div className="avatar-icon">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar4.png" />
+                            </div>
+                        </div>
+                        <div className="col-sm-9 col-xs-9 sideBar-main">
+                            <div className="row">
+                                <div className="col-sm-8 col-xs-8 sideBar-name">
+                                    <span className="name-meta">John Doe
+                                    </span>
+                                </div>
+                                <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+                                    <span className="time-meta pull-right">18:18
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a className="heading-name-meta" style={{ padding: '10px', borderBottom: '0.5px solid #ccc', backgroundColor: '#fbfbfb' }}>Groups</a>
+                    <div className="row sideBar-body">
+                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
+                            <div className="avatar-icon">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" />
+                            </div>
+                        </div>
+                        <div className="col-sm-9 col-xs-9 sideBar-main">
+                            <div className="row">
+                                <div className="col-sm-8 col-xs-8 sideBar-name">
+                                    <span className="name-meta">John Doe
+                                    </span>
+                                </div>
+                                <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+                                    <span className="time-meta pull-right">18:18
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row sideBar-body">
+                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
+                            <div className="avatar-icon">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar4.png" />
+                            </div>
+                        </div>
+                        <div className="col-sm-9 col-xs-9 sideBar-main">
+                            <div className="row">
+                                <div className="col-sm-8 col-xs-8 sideBar-name">
+                                    <span className="name-meta">John Doe
+                                    </span>
+                                </div>
+                                <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+                                    <span className="time-meta pull-right">18:18
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a className="heading-name-meta" style={{ padding: '10px', borderBottom: '0.5px solid #ccc', backgroundColor: '#fbfbfb' }}>Departments</a>
+                    <div className="row sideBar-body">
+                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
+                            <div className="avatar-icon">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar4.png" />
+                            </div>
+                        </div>
+                        <div className="col-sm-9 col-xs-9 sideBar-main">
+                            <div className="row">
+                                <div className="col-sm-8 col-xs-8 sideBar-name">
+                                    <span className="name-meta">John Doe
+                                    </span>
+                                </div>
+                                <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+                                    <span className="time-meta pull-right">18:18
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a className="heading-name-meta" style={{ padding: '10px', borderBottom: '0.5px solid #ccc', backgroundColor: '#fbfbfb' }}>Others</a>
                     <div className="row sideBar-body">
                         <div className="col-sm-3 col-xs-3 sideBar-avatar">
                             <div className="avatar-icon">
