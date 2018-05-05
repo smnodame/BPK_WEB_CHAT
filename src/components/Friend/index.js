20e6bcef8aa7e48e77219642e4d0545b6892983d
boonprakit <boonprakit@proteus-tech.com>
2018-05-05 16:48:11 +0700

    remove time in contact

--- src/components/Friend/index.js
+++ src/components/Friend/index.js
@@ -17,7 +17,7 @@ const Friend = ({ image, name, status }) => {
                         </span>
                     </div>
                     <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
-                        <span className="time-meta pull-right">18:18
+                        <span className="time-meta pull-right">
                         </span>
                     </div>
                 </div>
