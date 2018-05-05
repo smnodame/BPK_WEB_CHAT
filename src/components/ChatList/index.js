a715e3dfaf3e78ee1515ebf59e82050dde236f4e
boonprakit <boonprakit@proteus-tech.com>
2018-05-05 23:40:50 +0700

    can show chat list

--- src/components/ChatList/index.js
+++ src/components/ChatList/index.js
@@ -1,4 +1,7 @@
+import _ from 'lodash'
 import React from 'react'
+import moment from 'moment'
+
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 
@@ -6,6 +9,7 @@ class ChatList extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            chatLists: []
         }
 
         this.navigateToChat = () => {
@@ -17,9 +21,48 @@ class ChatList extends React.Component {
 
     }
 
+    componentWillReceiveProps() {
+        if(_.get(this.props.data, 'chat.chatLists')) {
+            this.setState({
+                chatLists: this.props.data.chat.chatLists
+            })
+        }
+    }
+
+    renderChatLists = () => {
+        const chatLists = this.state.chatLists.map((info) => {
+            return (
+                <div key={info.chat_room_id} className="row compose-sideBar">
+                    <div className="row sideBar-body">
+                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
+                            <div className="avatar-icon">
+                                <img src={info.profile_pic_url} />
+                            </div>
+                        </div>
+                        <div className="col-sm-9 col-xs-9 sideBar-main">
+                            <div className="row">
+                                <div className="col-sm-8 col-xs-8 sideBar-name">
+                                    <span className="name-meta"> { info.display_name }
+                                    </span>
+                                    <span className="status-meta"> { info.last_message }
+                                    </span>
+                                </div>
+                                <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+                                    <span className="time-meta pull-right"> {moment(info.last_chat).fromNow()}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+        return chatLists
+    }
+
     render = () => {
         return (
-            <div>
+            <div style={{ backgroundColor: 'white'}}>
                 <div className="row newMessage-heading">
                     <div className="row newMessage-main">
                         <div className="col-sm-2 col-xs-2 newMessage-back">
@@ -39,49 +82,12 @@ class ChatList extends React.Component {
                         </div>
                     </div>
                 </div>
-
-                <div className="row compose-sideBar">
-                    <div className="row sideBar-body">
-                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
-                            <div className="avatar-icon">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" />
-                            </div>
-                        </div>
-                        <div className="col-sm-9 col-xs-9 sideBar-main">
-                            <div className="row">
-                                <div className="col-sm-8 col-xs-8 sideBar-name">
-                                    <span className="name-meta">John Doe
-                                    </span>
-                                </div>
-                                <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
-                                    <span className="time-meta pull-right">18:18
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="row sideBar-body">
-                        <div className="col-sm-3 col-xs-3 sideBar-avatar">
-                            <div className="avatar-icon">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" />
-                            </div>
-                        </div>
-                        <div className="col-sm-9 col-xs-9 sideBar-main">
-                            <div className="row">
-                                <div className="col-sm-8 col-xs-8 sideBar-name">
-                                    <span className="name-meta">John Doe
-                                    </span>
-                                </div>
-                            <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
-                                <span className="time-meta pull-right">18:18
-                                </span>
-                            </div>
-                        </div>
-                    </div>
+                <div className="row sideBar">
+                    {
+                        this.renderChatLists()
+                    }
                 </div>
             </div>
-        </div>
         )
     }
 }
