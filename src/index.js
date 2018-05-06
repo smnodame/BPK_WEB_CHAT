19be1f072b740f74fa04a5851787e56618a998ef
boonprakit <boonprakit@proteus-tech.com>
2018-05-06 23:22:26 +0700

    add toast css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { makeMainRoutes } from './routes';
 import { store } from './redux'
 import { start_app  } from './redux/actions.js'
 
+import 'react-toastify/dist/ReactToastify.css'
+
 const routes = makeMainRoutes();
 
 // call first action that should do
