268a96ec67f7421ef89388fee434a30dcf053925
boonprakit <boonprakit@proteus-tech.com>
2018-05-29 01:42:49 +0700

    when call api with post method should use content type as application/x-www-form-urlencoded; charset=UTF-8

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,6 +2,18 @@ import axios from "axios"
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    if(config.method == 'post') {
+        config.headers['Content-Type'] =  'application/x-www-form-urlencoded; charset=UTF-8' 
+    }
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+})
+
 export const getAuth = () => {
     return new Promise(function(resolve, reject) {
         const user_id = localStorage.getItem("user_id") || '3963'
